6d75a0020207b884a597f368c28286ffdf656718
shourovr82 <shafinur512@gmail.com>
2023-08-20 23:51:21 +0600

    [DONE] Assigning API Endpoint Permissions by Role

--- src/app/modules/student/student.routes.ts
+++ src/app/modules/student/student.routes.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { StudentController } from './student.controller';
 import { StudentValidation } from './student.validations';
@@ -8,15 +10,21 @@ const router = express.Router();
 router.get('/', StudentController.getAllFromDB);
 
 router.get('/:id', StudentController.getByIdFromDB);
-router.delete('/:id', StudentController.deleteStudent);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  StudentController.deleteStudent
+);
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(StudentValidation.update),
   StudentController.updateIntoDB
 );
 
 router.post(
   '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(StudentValidation.create),
   StudentController.insertIntoDB
 );
