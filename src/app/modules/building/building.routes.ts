db5da16821c92bbc4740ee491172c24d61d06367
shourovr82 <shafinur512@gmail.com>
2023-08-21 13:37:49 +0600

    [DONE] Validation for Building Creation and Role-based API Access

--- src/app/modules/building/building.routes.ts
+++ src/app/modules/building/building.routes.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { BuildingController } from './building.controller';
+import { BuildingValidation } from './building.validation';
 const router = express.Router();
 
-router.post('/', BuildingController.insertIntoDB);
 router.get('/', BuildingController.getAllFromDB);
+router.post(
+  '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  validateRequest(BuildingValidation.create),
+  BuildingController.insertIntoDB
+);
 
 export const BuildingRoutes = router;
