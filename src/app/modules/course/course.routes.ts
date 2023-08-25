7da2d138038be1f4620f3b3a2514b3777ff7661e
shourovr82 <shafinur512@gmail.com>
2023-08-25 10:41:00 +0600

    [DONE] course get all, course delete and course get by id completed service

--- src/app/modules/course/course.routes.ts
+++ src/app/modules/course/course.routes.ts
@@ -4,10 +4,17 @@ import auth from '../../middlewares/auth';
 import { CourseController } from './course.controller';
 const router = express.Router();
 
+router.get('/', CourseController.getAllFromDB);
+router.get('/:id', CourseController.getByIdFromDB);
+
 router.post(
   '/',
   auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   CourseController.insertIntoDB
 );
-
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  CourseController.deleteByIdFromDB
+);
 export const CourseRoutes = router;
