e7bc940ef25cd1dc693c6bd0052bcd72fa49fbd7
shourovr82 <shafinur512@gmail.com>
2023-08-29 10:54:15 +0600

    [DONE] Data Insertion for Offered Course Section

--- src/app/modules/offeredCourseSection/offeredCourseSection.service.ts
+++ src/app/modules/offeredCourseSection/offeredCourseSection.service.ts
@@ -1,10 +1,22 @@
 import { OfferedCourseSection } from '@prisma/client';
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
 import prisma from '../../../shared/prisma';
 
-const insertIntoDB = async (
-  data: OfferedCourseSection
-): Promise<OfferedCourseSection> => {
+const insertIntoDB = async (data: any): Promise<OfferedCourseSection> => {
   //
+  const isExistOfferedCourse = await prisma.offeredCourse.findFirst({
+    where: {
+      id: data.offeredCourseId,
+    },
+  });
+
+  if (!isExistOfferedCourse) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Offered Course Does not Exist');
+  }
+
+  data.semesterRegistrationId = isExistOfferedCourse.semesterRegistrationId;
+
   const result = await prisma.offeredCourseSection.create({
     data,
   });
